f9253a1a6a9655b7e3fb7744b1f7aa8b290703d7
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-01-24 15:16:08 -0500

    update

--- src/insmet.js
+++ src/insmet.js
@@ -1,22 +1,19 @@
-var cheerio = require('cheerio');  
+const cheerio = require('cheerio');  
 var rp = require('request-promise');
 
 exports.handler = (event, context, callback) => {
-	let response, body;
-
-    rp('http://www.insmet.cu/asp/link.asp?PRONOSTICO')
+    rp('http://www.insmet.cu/asp/genesis.asp?TB0=PLANTILLAS&TB1=PT&TB2=/Pronostico/pttn.txt')
         .then( html => {
-            let $ = cheerio.load( html );
-            let reDate = /Fecha:\s(\d+\s.+\d{4}).+Hora:\s(\d+:\d{2}\s[a|p]\.m)/;
+            const $ = cheerio.load( html );
+            const reDate = /Fecha:\s(\d+\s.+\d{4}).+Hora:\s(\d+:\d{2}\s[a|p]\.m)/;
             let dateToday = $('table.contenidoPagina').text().match( reDate );
             callback(null, {
                 headers: { 
-                    'Content-Type':'application/json' , 
-                    'Access-Control-Allow-Origin': '*'
+                    'Content-Type':'application/json; charset=utf-8' , 
                 },    	
                 statusCode: 200,
         	    body: JSON.stringify({
-                    updated_at: dateToday[1]+" "+dateToday[2],
+                    updated_at: dateToday[1] + " " + dateToday[2],
                     content: $('table.contenidoPagina').text().replace(/\s{2,}/g,'')
                 })
             })
